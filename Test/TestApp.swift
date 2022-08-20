6ee8dadb8d61462ea9af7f5d4c0cac6a7a81c464
saneesh antony <saneeshantony00@gmail.com>
2022-08-20 15:09:43 -0300

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by saneesh antony on 2022-08-20.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
